f45f51c5d25af1fcb4e15a0e29f1c8001a55058c
KieranOCallaghan-cork <g00412669@atu.ie>
2024-09-30 13:54:01 +0000

    Commented and Ex3 Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+//This imports all the contet from the components folder 
 
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+// main part of the app 
 function App() {
   return (
+    //this is where the conent is displayed 
     <div>
-      <h1>Hello World!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
-
+//ends with it been exportred
 export default App;
