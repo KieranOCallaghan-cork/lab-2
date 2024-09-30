8e3dae97cd1f5e63160bf4967e17fef153e7ed59
KieranOCallaghan-cork <g00412669@atu.ie>
2024-09-30 14:00:40 +0000

    ex4.2

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,5 +1,7 @@
+//made the start of the code calling on the constant 
 const Header = () => {
-    return <h1>My Header in another component</h1>;
+    //made it so the text would be center and a bit of text 
+    return <h1 className="text-center">My Header in another component</h1>;
   };
   
   export default Header;
