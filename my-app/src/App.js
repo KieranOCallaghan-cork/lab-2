5c4d9a75a2d74bc8b83635a34d96fa9ce0bb7dd0
KieranOCallaghan-cork <g00412669@atu.ie>
2024-09-30 14:08:00 +0000

    All Commeded part of code (done)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
-//This imports all the contet from the components folder 
 
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
-//addimg a nav bar to the app
-import Header from './components/NavigationBar';
+//This imports all the contet from the components folder and the routor 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Content from './components/Content';
 // main part of the app 
 function App() {
+  //this is where the conent is displayed 
+    //also added the nav bar to the code with the routes been changed too
   return (
-    //this is where the conent is displayed 
-    //also added the nav bar to the code 
-    <div>
+    <Router>
       <NavigationBar />
-      <Header />
-      <Content />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 //ends with it been exportred
 export default App;
+
