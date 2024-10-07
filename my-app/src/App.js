db91d710744f1345a725f35bccd06ca62a6b610b
KieranOCallaghan-cork <g00412669@atu.ie>
2024-10-07 13:47:33 +0000

    this is change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Content from './components/Content';
-import Read from './components/read';
+import Read from './components/Read';
 import Create from './components/Create';
 
 // main part of the app 
